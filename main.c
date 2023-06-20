636d5275cfb0779c7a8724cb47df7194fbe8bc33
CarnisJr <mirandab@gmail.com>
2023-06-20 17:23:59 -0500

    Número polindromo: main.c

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main(void){
         
-    int num, aux, fact, selector;
-    int numPalindromo[5], numPalindromoInverso[5], cont = 0;
-
+    
+    int num, numInverso;
+    int i, j, aux, aux1, cont, fact, selector; 
 
     do{
+
         fact = 1;
         printf("Menu\n");
-        printf("1. For\n2. Do While\n3. While\n4. Palindromo\n10. Salir\n\n");
+        printf("1. For\n2. Do While\n3. While\n4. Palindromo\n5. Siguiente\n6. Salir\n\n");
         scanf("%d", &selector);
+        system("cls");
 
-        switch (selector)
-        {
+        switch (selector){
+
         case 1:
+
             printf("Con for\n\n");
             printf("Ingrese un numero entero positivo: ");
             scanf("%d", &num);
@@ -27,8 +31,8 @@ int main(void){
             printf("El factorial de %d es: %d\n", num, fact);
             system("pause");
             system("cls");
-
             break;
+
         case 2:
             printf("Con do while\n\n");
             printf("Ingrese un numero entero positivo: ");
@@ -42,9 +46,10 @@ int main(void){
             printf("El factorial de %d es: %d\n", num, fact);
             system("pause");
             system("cls");
-
             break;
+
         case 3:
+
             printf("Con While\n\n");
             printf("Ingrese un numero entero positivo: ");
             scanf("%d", &num);
@@ -61,51 +66,68 @@ int main(void){
         
         case 4:
             
-            for(int i = 0; i < 5; i++){
+            printf("Numero Palindromo\n\n");
+            numInverso = 0;
+            cont = 0;
+            i = 1;
+            j = 1;
+            printf("Ingresar un numero: ");
+            scanf("%d", &num);
+            aux = num;
 
-                printf("Ingresa el primer digito del numero: ");
-                scanf("%d", &num);
-                numPalindromo[i] = num;
-                numPalindromoInverso[5-i] = num;
+            //Contador de digitos
+            while(aux > 0){
+
+                aux = aux / 10;
+                cont++;
             }
-            
-            for(int i = 0; i < 5; i++){
-
-                printf("%d", numPalindromo[i]); 
-            }
-
-            printf("\n");
-
-            for(int i = 1; i <= 5; i++){
-
-                printf("%d", numPalindromoInverso[i]); 
-            }
-
-            printf("\n");
-
-
-            for(int i = 0; i < 5; i++){
-
-                if(numPalindromo[i] == numPalindromo[4-i])
-                    cont++;
+            while(cont - 1 > 0){
+                i *= 10;
+                cont--;
             }
 
-            if(cont == 5)
-                printf("Es Palidromo\n");
-            else
-                printf("No es Palidromo\n");
+            for(i; i > 0; i /= 10){
 
+                aux = num / i;
+                aux1 = aux % 10;
+                numInverso += (aux1 * j);
+                j *= 10;
+            }
+
+            printf("%d\n", numInverso);
+
+            if(num == numInverso){
+
+                printf("ES PALINDROMO\n");
+            }else{
+                printf("No es palindromo\n");
+            }
+
+            system("pause");
+            system("cls");
+            break;
+
+        case 5:
+
+            /*Codigo del problema del viernes*/
+            continue;
+            break;
+
+        case 6:
+
+            printf("SALIENDO...");
+            system("pause");
+            system("cls");
             break;
 
         default:
-            printf("ERROR");
+
+            printf("ERROR\n\n");
+            system("pause");
+            system("cls");
             break;
         }
+    }while(selector != 6);
 
-
-    }while(selector != 10);
-
-    
-    system("pause");
     return 0;
 }
