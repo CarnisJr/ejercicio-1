724fc5b2a24515c26f1c59f4ca1f120def4291e6
CarnisJr <mirandab@gmail.com>
2023-06-20 12:04:48 -0500

    Palidromo que no funciona

--- main.c
+++ main.c
@@ -4,12 +4,13 @@
 int main(void){
         
     int num, aux, fact, selector;
+    int numPalindromo[5], numPalindromoInverso[5], cont = 0;
 
 
     do{
         fact = 1;
         printf("Menu\n");
-        printf("1. For\n2. Do While\n3. While\n4. Salir\n\n");
+        printf("1. For\n2. Do While\n3. While\n4. Palindromo\n10. Salir\n\n");
         scanf("%d", &selector);
 
         switch (selector)
@@ -59,16 +60,50 @@ int main(void){
             break;
         
         case 4:
-            continue;
-            break;
             
+            for(int i = 0; i < 5; i++){
+
+                printf("Ingresa el primer digito del numero: ");
+                scanf("%d", &num);
+                numPalindromo[i] = num;
+                numPalindromoInverso[5-i] = num;
+            }
+            
+            for(int i = 0; i < 5; i++){
+
+                printf("%d", numPalindromo[i]); 
+            }
+
+            printf("\n");
+
+            for(int i = 1; i <= 5; i++){
+
+                printf("%d", numPalindromoInverso[i]); 
+            }
+
+            printf("\n");
+
+
+            for(int i = 0; i < 5; i++){
+
+                if(numPalindromo[i] == numPalindromo[4-i])
+                    cont++;
+            }
+
+            if(cont == 5)
+                printf("Es Palidromo\n");
+            else
+                printf("No es Palidromo\n");
+
+            break;
+
         default:
             printf("ERROR");
             break;
         }
 
 
-    }while(selector != 4);
+    }while(selector != 10);
 
     
     system("pause");
